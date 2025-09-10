5fa5142c7223c436791538314129c605f27f1281
saikirangoka <gokasaikiran15@gmail.com>
2025-09-10 10:51:55 +0530

    Vpc_module

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
 /* output "azs_info" {
   value = slice(data.aws_availability_zones.main.names, 0, 2)
 } */
+
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
